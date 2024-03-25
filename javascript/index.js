66fe30d5ee780573a50249036f90370a73bae9c3
AnderesNubxd <chainsaw654@gmail.com>
2024-03-24 23:17:33 -0400

    Agragar Tarjetas

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+function AgregarTarjeta() {
+    const imagen = document.createElement("img");
+    imagen.src = "https://img.freepik.com/fotos-premium/gatitos-kawaii-peluche-esponjosos-hermosa-imagen-arte-generado-ai_843679-5987.jpg";
+    const espacioDeTexto = document.createElement("p");
+    const texto = document.createTextNode("Los gatitos hacen miau")
+    espacioDeTexto.appendChild(texto)
+    imagen.appendChild(espacioDeTexto)
+    return imagen
+}
+
+const contenedorPrincipal = document.querySelector(".nuevasTarjetas");
+
+function alPrecionar() {
+    contenedorPrincipal.appendChild(AgregarTarjeta());
+}
